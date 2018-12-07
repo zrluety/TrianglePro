2a26c7713af901d547aaf046c6c9bf21a30cf35f
Zachary Luety <zrluety216@gmail.com>
2018-12-06 19:03:19 -0700

    generalized link ratio calculations

--- tests/test_loss_development.py
+++ tests/test_loss_development.py
@@ -5,8 +5,17 @@ from trianglepro.reserve import loss_development
 
 def test_link_ratio():
     values = np.array([[1.0, 2.0], [1.0, np.NaN]])
-    np.testing.assert_equal(loss_development.calculate_link_ratio(values), np.array([2., np.NaN]))
+    np.testing.assert_equal(loss_development.calculate_link_ratios(values), np.array([[2.], [np.NaN]]))
 
 def test_link_ratio_three_cols():
-    values = np.array([[1.0, 2.0, 2.5], [1.0, 2.0, np.NaN], [1.0, 2.0, np.NaN]])
-    np.testing.assert_equal(loss_development.calculate_link_ratio(values), np.array([[2., 1.25], [2.0, np.NaN], [np.NaN], np.NaN]))
+    values = np.array([[1.0, 2.0, 2.5], [1.0, 2.0, np.NaN], [1.0, np.NaN, np.NaN]])
+    expected = np.array([[2., 1.25], [2.0, np.NaN], [np.NaN, np.NaN]])
+    np.testing.assert_equal(loss_development.calculate_link_ratios(values), expected)
+
+
+def test_weighted_average_link_ratio():
+    values = np.array([[1., 2., ], [1., 1.5]])
+    link_ratios = np.array([[2.0], [1.5]])
+
+    expected = np.array([1.75])
+    np.testing.assert_equal(loss_development.calculate_weighted_average_link_ratio(link_ratios, values), expected)
--- trianglepro/reserve/loss_development.py
+++ trianglepro/reserve/loss_development.py
@@ -1,6 +1,31 @@
+import itertools
+
 import numpy as np
 
-def calculate_link_ratio(values: np.ndarray):
-    p1 = values[:, 0]
-    p2 = values[:, 1]
-    return np.divide(p2, p1)
+
+def calculate_link_ratios(values: np.ndarray):
+    """Calculate the link ratios for an array of values.
+
+    :param values: an array of values of shape (m, n).
+    :return: an array of link ratios of shape (m, n-1).
+    """
+    link_ratios = np.array([np.divide(period2, period1) for period1, period2 in _pairwise(values.T)])
+    return link_ratios.T
+
+
+def calculate_weighted_average_link_ratio(link_ratios: np.ndarray, values: np.ndarray):
+    """
+
+    :param link_ratios: an array of link ratios of shape (m, n-1)
+    :param values: an array of values of shape (m, n) used as weights.
+    :return: a 1d array of weighted average link_ratios
+    """
+    return np.average(link_ratios, axis=0, weights=values[:, 0])
+
+
+def _pairwise(iterable):
+    # taken from Itertools Recipies in Python 3 docs
+    # https://docs.python.org/3/library/itertools.html
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
